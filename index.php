61033b466b3e5a331bdacfab9256261630fc1072
Porolilepishechka <124508160+Porolilepishechka@users.noreply.github.com>
2023-04-03 18:27:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+ require("header.html")
+?>
+
+<?php
+
+ $url = "https://api.openweathermap.org/data/2.5/weather";
+ $options = array(
+    'id' => 706369,
+    'APPID' => 'f4a1d82f0ac16a971f10a3017ebe86c5',
+    'units' => 'metric',
+    'lang' => 'en',
+ );
+
+ $ch = curl_init();
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
+
+ $response = curl_exec($ch);
+ $data = json_decode($response, true);
+
+ curl_close($ch);
+
+ if($data['weather']['0']['main'] == 'Clouds'){
+  $weather = "weather-clouds.jpg";
+  echo '<div class="img-weather"><img src='. $weather. '></div>';
+ }
+ else if($data['weather']['0']['main'] == 'Rain'){
+  $weather = "weather-rain.jpg";
+  echo '<div class="img-weather"><img src='. $weather. '></div>';
+ }
+ else if($data['weather']['0']['main'] == 'Sun'){
+  $weather = "weather-sun.jpg";
+  echo '<div class="img-weather"><img src='. $weather. '></div>';
+ }
+ else if($data['weather']['0']['main'] == 'Snow'){
+  $weather = "weather-Snow.jpg";
+  echo '<div class="img-weather"><img src='. $weather. '></div>';
+ }
+ else if($data['weather']['0']['main'] == 'Thunderstorm'){
+  $weather = "weather-thunderstorm.jpg";
+  echo '<div class="img-weather"><img src='. $weather. '></div>';
+ }
+
+
+ $temp = array_slice($data, 3, true);
+ echo '<h2>'.'°C '.$temp['main']['temp'].'</h2>';
+ echo '<h2>'.'wind speed: '.$data['wind']['speed'].' m/s'.'</h2>';
+ echo '<h2>'.'weather: '.$data['weather']['0']['main'].'</h2>';
+ echo '<h2>'.'description: '.$data['weather']['0']['description'].'</h2>';
+
+?>
+
+<?php
+  require("footer.html")
+?>
